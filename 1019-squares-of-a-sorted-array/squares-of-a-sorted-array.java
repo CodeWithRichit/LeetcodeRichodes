535d89ae43d707a0f1534083740311f5f6af7993
CodeWithRichit <richittyagi@gmail.com>
2026-07-05 16:10:09 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (86.81%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,22 +1,39 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        for(int i=0;i<nums.length-1;i++){
-            int swaps=0;
-            for(int j=0;j<nums.length-1-i;j++){
-                if(nums[j]>nums[j+1]){
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                    swaps++;
-                }
+        int n=nums.length;
+        int[] ans=new int[n];
+        int i=0,j=n-1,pos=n-1;
+        while(i<=j){
+            int sqi=nums[i]*nums[i];
+            int sqj=nums[j]*nums[j];
+            if(sqi>sqj){
+               ans[pos]=sqi;
+               i++;
             }
-            if(swaps==0){
-                break;
+            else{
+                ans[pos]=sqj;
+                j--;
             }
+            pos--;
         }
-        return nums;
+        return ans;
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // for(int i=0;i<nums.length-1;i++){
+        //     int swaps=0;
+        //     for(int j=0;j<nums.length-1-i;j++){
+        //         if(nums[j]>nums[j+1]){
+        //             int temp=nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //             swaps++;
+        //         }
+        //     }
+        //     if(swaps==0){
+        //         break;
+        //     }
+        // }
+        // return nums;
     }
 }
