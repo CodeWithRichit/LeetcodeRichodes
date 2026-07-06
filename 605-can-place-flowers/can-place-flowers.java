961a1e419d1e9f6cb3cc20778da5052f087ed3c0
CodeWithRichit <richittyagi@gmail.com>
2026-07-07 04:28:36 +0530

    Time: 1 ms (98.85%) | Memory: 47.8 MB (36.18%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -11,8 +11,7 @@ class Solution {
                 return true;
             }
         }
-        while(n-->0){
-           for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i]==0){
                if((i-1<0 || arr[i-1]==0) && (i+1==arr.length || arr[i+1]==0)){
                 arr[i]=1;
@@ -20,7 +19,6 @@ class Solution {
                }
             }
            }
-        }
         if(count>=m){
             return true;
         }
