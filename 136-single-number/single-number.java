301c3b82bcd2048ece7fcdfbd1c758af399a2237
CodeWithRichit <richittyagi@gmail.com>
2026-07-23 04:20:24 +0530

    Time: 14 ms (15.69%) | Memory: 47.4 MB (6.82%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,25 +1,41 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=-1;
-        for(int i=0;i<nums.length;i++){
-            int r=1;
-            int l=1;
-           for(int j=0;j<i;j++){
-            if(nums[i]==nums[j]){
-                r=0;
-                break;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans=1;
+        for(int e:nums){
+            if(map.containsKey(e)){
+                int f=map.get(e);
+                map.put(e,f+1);
             }
-           }
-           for(int k=i+1;k<nums.length;k++){
-             if(nums[i]==nums[k]){
-                l=0;
-                break;
+            else{
+                map.put(e,1);
             }
-           }
-           if(r==1 && l==1){
-            ans=nums[i];
-           }
         }
+        for(int e:map.keySet()){
+            if(map.get(e)==1){
+                ans=e;
+            }
+        }
+        // int ans=-1;
+        // for(int i=0;i<nums.length;i++){
+        //     int r=1;
+        //     int l=1;
+        //    for(int j=0;j<i;j++){
+        //     if(nums[i]==nums[j]){
+        //         r=0;
+        //         break;
+        //     }
+        //    }
+        //    for(int k=i+1;k<nums.length;k++){
+        //      if(nums[i]==nums[k]){
+        //         l=0;
+        //         break;
+        //     }
+        //    }
+        //    if(r==1 && l==1){
+        //     ans=nums[i];
+        //    }
+        // }
         return ans;
     }
 }
