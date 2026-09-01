e5b20898aec048d7afd323dd68efe8c950465d58
CodeWithRichit <richittyagi@gmail.com>
2026-09-01 10:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (67.15%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,30 +10,56 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ArrayList<ListNode> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp);
+        ListNode dummy=new ListNode(-1);
+        ListNode t=dummy;
+        dummy.next=head;
+        for(int i=1;i<left;i++){
+            t=t.next;
+        }
+        ListNode c=t.next;
+        ListNode h1=c;
+        ListNode temp=t.next;
+        t.next=null;
+        ListNode p=null;
+        ListNode f=null;
+        for(int i=left;i<right;i++){
             temp=temp.next;
         }
-        int i=left-1;
-        int j=right-1;
-        while(i<=j){
-            ListNode a=arr.get(i);
-            ListNode b=arr.get(j);
-            ListNode t=a;
-            arr.set(i,b);
-            arr.set(j,t);
-            i++;
-            j--;
+        ListNode h2=temp.next;
+        temp.next=null;
+        while(c!=null){
+            f=c.next;
+            c.next=p;
+            p=c;
+            c=f;
         }
-        for(i=0;i<arr.size();i++){
-            if(i==arr.size()-1){
-                arr.get(i).next=null;
-            }
-            else{
-            arr.get(i).next=arr.get(i+1);}
-        }
-        return arr.get(0);
+        t.next=p;
+        h1.next=h2;
+        return dummy.next;
+        // ArrayList<ListNode> arr=new ArrayList<>();
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     arr.add(temp);
+        //     temp=temp.next;
+        // }
+        // int i=left-1;
+        // int j=right-1;
+        // while(i<=j){
+        //     ListNode a=arr.get(i);
+        //     ListNode b=arr.get(j);
+        //     ListNode t=a;
+        //     arr.set(i,b);
+        //     arr.set(j,t);
+        //     i++;
+        //     j--;
+        // }
+        // for(i=0;i<arr.size();i++){
+        //     if(i==arr.size()-1){
+        //         arr.get(i).next=null;
+        //     }
+        //     else{
+        //     arr.get(i).next=arr.get(i+1);}
+        // }
+        // return arr.get(0);
     }
 }
