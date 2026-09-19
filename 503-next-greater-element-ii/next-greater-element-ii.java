67634a67dc43ee5f69e533e9380872c840f0a99e
CodeWithRichit <richittyagi@gmail.com>
2026-09-19 12:26:41 +0530

    Time: 14 ms (63.85%) | Memory: 48.6 MB (34.90%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,25 +4,43 @@ class Solution {
         for(int i=nums.length-1;i>=0;i--){
             st.push(nums[i]);
         }
-        int[] arr=new int[nums.length];
+        int[] ans=new int[nums.length];
         for(int i=nums.length-1;i>=0;i--){
-            if(st.peek()>nums[i]){
-                arr[i]=st.peek();
-                st.push(nums[i]);
+            while(st.size()>0 && st.peek()<=nums[i]){
+                st.pop();
+            }
+            if(st.size()!=0){
+                ans[i]=st.peek();
             }
             else{
-                while(st.size()!=0&&st.peek()<=nums[i]){
-                    st.pop();
-                }
-                if(st.size()==0){
-                    arr[i]=-1;
-                }
-                else{
-                     arr[i]=st.peek();  
-                }
-                st.push(nums[i]);
+                ans[i]=-1;
             }
+            st.push(nums[i]);
         }
-        return arr;
+        return ans;
+    //     Stack<Integer> st=new Stack<>();
+    //     for(int i=nums.length-1;i>=0;i--){
+    //         st.push(nums[i]);
+    //     }
+    //     int[] arr=new int[nums.length];
+    //     for(int i=nums.length-1;i>=0;i--){
+    //         if(st.peek()>nums[i]){
+    //             arr[i]=st.peek();
+    //             st.push(nums[i]);
+    //         }
+    //         else{
+    //             while(st.size()!=0&&st.peek()<=nums[i]){
+    //                 st.pop();
+    //             }
+    //             if(st.size()==0){
+    //                 arr[i]=-1;
+    //             }
+    //             else{
+    //                 arr[i]=st.peek();  
+    //             }
+    //             st.push(nums[i]);
+    //         }
+    //     }
+    //     return arr;
     }
 }
