32206bac3006653776c71c01577f7bd3cb0c0bf9
CodeWithRichit <richittyagi@gmail.com>
2026-09-19 11:23:41 +0530

    Time: 4 ms (73.41%) | Memory: 44.9 MB (76.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -5,33 +5,58 @@ class Solution {
         int[] arr=new int[nums2.length];
         arr[arr.length-1]=-1;
         for(int i=nums2.length-2;i>=0;i--){
-            if(nums2[i]<st.peek()){
-                arr[i]=st.peek();
-                st.push(nums2[i]);         
+            while(st.size()>0 && st.peek()<=nums2[i]){
+                st.pop();
             }
+            if(st.size()==0)arr[i]=-1;
             else{
-                while(st.size()!=0 && nums2[i]>st.peek()){
-                    st.pop();
-                }
-                if(st.size()==0){
-                    arr[i]=-1;
-                }
-                else{
-                    arr[i]=st.peek();
-                }
-                st.push(nums2[i]);
+                arr[i]=st.peek();
             }
+            st.push(nums2[i]);
         }
         int pos=0;
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
-                    nums1[pos]=arr[j];
-                    pos++;
-                    break;
+                  nums1[pos]=arr[j];
+                  pos++;
+                  break;
                 }
             }
         }
         return nums1;
+        // Stack<Integer> st=new Stack<>();
+        // st.push(nums2[nums2.length-1]);
+        // int[] arr=new int[nums2.length];
+        // arr[arr.length-1]=-1;
+        // for(int i=nums2.length-2;i>=0;i--){
+        //     if(nums2[i]<st.peek()){
+        //         arr[i]=st.peek();
+        //         st.push(nums2[i]);         
+        //     }
+        //     else{
+        //         while(st.size()!=0 && nums2[i]>st.peek()){
+        //             st.pop();
+        //         }
+        //         if(st.size()==0){
+        //             arr[i]=-1;
+        //         }
+        //         else{
+        //             arr[i]=st.peek();
+        //         }
+        //         st.push(nums2[i]);
+        //     }
+        // }
+        // int pos=0;
+        // for(int i=0;i<nums1.length;i++){
+        //     for(int j=0;j<nums2.length;j++){
+        //         if(nums1[i]==nums2[j]){
+        //             nums1[pos]=arr[j];
+        //             pos++;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return nums1;
     }
 }
